8901716e50f7450bb50719fc51ee35cacdc84a13
LeelaMohan217 <leelamohan217@gmail.com>
2026-08-05 22:12:42 +0530

    Decrease gap between Who We Are heading and image/content

--- src/pages/home/IntroSection.jsx
+++ src/pages/home/IntroSection.jsx
@@ -10,7 +10,7 @@ const IntroSection = () => {
       id="who-we-are"
       className="relative overflow-hidden bg-white py-10 md:py-14 scroll-mt-36"
     >
-      <div className="max-w-6xl mx-auto px-6 md:px-8 flex flex-col gap-14">
+      <div className="max-w-6xl mx-auto px-6 md:px-8 flex flex-col gap-8">
         <SectionHeading
           eyebrow="Who We Are"
           title="Building the ecosystem behind digital growth"
