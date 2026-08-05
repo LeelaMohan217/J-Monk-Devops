a3ea7e8cd9c9dcf014bed7ef42e70cbc62e1f658
LeelaMohan217 <leelamohan217@gmail.com>
2026-08-05 23:22:02 +0530

    Move hero background image further down

--- src/pages/home/HeroSection.jsx
+++ src/pages/home/HeroSection.jsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
         src={homeImages.heroRed}
         alt=""
         aria-hidden="true"
-        className="pointer-events-none absolute inset-0 h-full w-full object-cover object-[center_70%] [transform:scaleX(-1)]"
+        className="pointer-events-none absolute inset-0 h-full w-full object-cover object-bottom [transform:scaleX(-1)]"
       />
       <div className="absolute inset-0 bg-white/80" />
       <div className="relative z-10 max-w-6xl mx-auto px-6 md:px-8 py-24 md:py-32 min-h-[calc(100vh-4rem)] flex flex-col items-center justify-center text-center">
